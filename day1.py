1716fab5cf90f73230d935b10384ff2c235916c6
jirikuchta <jiri.kuchta@firma.seznam.cz>
2020-12-01 19:56:04 +0100

    day1

---
+++ day1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+def get_input():
+    with open("inputs/day1.txt") as reader:
+        return [int(i) for i in reader.readlines()]
+
+
+def part1(numbers):
+    for i, n1 in enumerate(numbers):
+        for n2 in numbers[i+1:]:
+            if n1+n2 == 2020:
+                return n1*n2
+
+
+def part2(numbers):
+    for i, n1 in enumerate(numbers):
+        for i2, n2 in enumerate(numbers[i+1:]):
+            for n3 in numbers[i2+1:]:
+                if n1+n2+n3 == 2020:
+                    return n1*n2*n3
+
+
+if __name__ == "__main__":
+    input = get_input()
+    print(part1(input))
+    print(part2(input))
