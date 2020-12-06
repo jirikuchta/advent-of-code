f1972dbca96b1d5da4c4501c4ba56c72b6001a10
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2020-12-06 20:28:24 +0100

    edit day5

--- day5.py
+++ day5.py
@@ -8,7 +8,8 @@ def get_input():
     return ((line[:7], line[7:].strip()) for line in open("inputs/day5.txt"))
 
 
-def count(input, min, max):
+def count(input, max):
+    min = 0
     for char in input:
         if char in ("F", "L"):
             max = min + ((max - min) // 2)
@@ -21,8 +22,8 @@ def part1():
     result = 0
 
     for item in get_input():
-        row = count(item[0], 0, 127)
-        column = count(item[1], 0, 7)
+        row = count(item[0], 127)
+        column = count(item[1], 7)
         result = max(row * 8 + column, result)
 
     return result
@@ -30,25 +31,20 @@ def part1():
 
 def part2():
     seats_grid = [[False] * 8 for i in range(128)]
-    taken_seats_ids = []
+    seats_ids = []
 
     for item in get_input():
-        row = count(item[0], 0, 127)
-        column = count(item[1], 0, 7)
+        row = count(item[0], 127)
+        column = count(item[1], 7)
         seats_grid[row][column] = True
-        taken_seats_ids.append(row * 8 + column)
+        seats_ids.append(row * 8 + column)
 
     for i, row in enumerate(seats_grid):
-        if all(row):
-            continue
-
-        for j, seat in enumerate(row):
-            if seat:
-                continue
-
-            seat_id = i * 8 + j
-            if seat_id + 1 in taken_seats_ids and seat_id - 1 in taken_seats_ids:
-                return seat_id
+        for j, taken in enumerate(row):
+            if not taken:
+                id = i * 8 + j
+                if id + 1 in seats_ids and id - 1 in seats_ids:
+                    return id
 
 
 print(part1())
