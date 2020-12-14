c3fd91bd5683d5eac8bdff36ff7fac95cc6ac813
jirikuchta <jiri.kuchta@firma.seznam.cz>
2020-12-14 20:42:31 +0100

    day 14

--- 14/solve.py
+++ 14/solve.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-# https://adventofcode.com/2020/day/14
+# https://adventofcode.com/…/14
 
 
-def part1():
-    mem = {}
+def read_input():
     mask = None
-
     for line in open("input.txt").readlines():
         if line.startswith("mask"):
             mask = [ch for ch in line[6:].strip()][::-1]
@@ -13,17 +11,46 @@ def part1():
         if line.startswith("mem"):
             bit = int(line[4:line.index("]")])
             value = int(line.split("=")[1].strip())
+            yield mask, bit, value
 
-            for i, ch in enumerate(mask):
-                if ch == "1":
-                    value = value | (1 << i)
 
-                if ch == "0":
-                    value = value & ~(1 << i)
+def part1():
+    mem = {}
 
-            mem[bit] = value
+    for mask, bit, value in read_input():
+        for i, ch in enumerate(mask):
+            if ch == "1":
+                mem[bit] = value | (1 << i)
+
+            if ch == "0":
+                mem[bit] = value & ~(1 << i)
+
+    return sum(mem.values())
+
+
+def part2():
+    mem = {}
+
+    for mask, bit, value in read_input():
+        for i, ch in enumerate(mask):
+            if ch == "1":
+                bit = bit | (1 << i)
+
+        mem_bits = [bit]
+
+        for i, ch in enumerate(mask):
+            if ch == "X":
+                tmp = []
+                for mem_bit in mem_bits:
+                    tmp.append(mem_bit | (1 << i))
+                    tmp.append(mem_bit & ~(1 << i))
+                mem_bits += tmp
+
+        for mem_bit in set(mem_bits):
+            mem[mem_bit] = value
 
     return sum(mem.values())
 
 
 print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
