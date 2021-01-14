30b3fd92227dce01d141bbf0625714d7608cb82a
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-01-14 15:37:45 +0100

    day 24 part 2

--- 24/solve.py
+++ 24/solve.py
@@ -27,13 +27,60 @@ def parse_input():
                 tile[0] -= 1
             if tile_dir == "e":
                 tile[0] += 1
-        tiles.append([float(tile[0]), float(tile[1])])
-    return tiles
+        tiles.append(tile)
+
+    black_tiles = list(filter(lambda t: tiles.count(t) % 2 == 1, tiles))
+
+    return tiles, black_tiles
 
 
 def part1():
-    tiles = parse_input()
-    return len(list(filter(lambda t: tiles.count(t) == 1, tiles)))
+    tiles, black_tiles = parse_input()
+    return len(black_tiles)
+
+
+def part2(days):
+    tiles, black_tiles = parse_input()
+
+    def get_adjacent_tiles(tile):
+        adjacent_tiles = [
+            [tile[0] + 0.5, tile[1] + 1],
+            [tile[0] + 1, tile[1]],
+            [tile[0] + 0.5, tile[1] - 1],
+            [tile[0] - 0.5, tile[1] - 1],
+            [tile[0] - 1, tile[1]],
+            [tile[0] - 0.5, tile[1] + 1]]
+
+        adjacent_black_tiles = []
+        adjacent_white_tiles = []
+
+        for adjacent_tile in adjacent_tiles:
+            if adjacent_tile in black_tiles:
+                adjacent_black_tiles.append(adjacent_tile)
+            else:
+                adjacent_white_tiles.append(adjacent_tile)
+
+        return adjacent_white_tiles, adjacent_black_tiles
+
+    for day in range(0, days):
+        tmp_black_tiles = []
+
+        for tile in black_tiles:
+            adjacent_white_tiles, adjacent_black_tiles = get_adjacent_tiles(tile)
+
+            if len(adjacent_black_tiles) in (1, 2):
+                if tile not in tmp_black_tiles:
+                    tmp_black_tiles.append(tile)
+
+            for white_tile in adjacent_white_tiles:
+                if len(get_adjacent_tiles(white_tile)[1]) == 2:
+                    if white_tile not in tmp_black_tiles:
+                        tmp_black_tiles.append(white_tile)
+
+        black_tiles = tmp_black_tiles
+
+    return len(black_tiles)
 
 
 print(f"Part 1: {part1()}")
+print(f"Part 2: {part2(100)}")
