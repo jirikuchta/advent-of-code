6c7a7cf7645febc54fc936dc8a8bd061c9450ab1
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-01-19 10:46:17 +0100

    day 25 part 1

--- 25/solve.py
+++ 25/solve.py
@@ -2,23 +2,18 @@
 # https://adventofcode.com/2020/day/25
 
 
-def parse_input():
-    card_pk, door_pk = [int(l) for l in open("input.txt").readlines()]
-
-    card_loop_size = 0
-    while pow(7, card_loop_size, 20201227) != card_pk:
-        card_loop_size += 1
-
-    door_loop_size = 0
-    while pow(7, door_loop_size, 20201227) != door_pk:
-        door_loop_size += 1
-
-    return card_pk, door_pk, card_loop_size, door_loop_size
+def get_loop_size(pk):
+    value = 1
+    for loop_size in range(1, 20201227):
+        value = (value * 7) % 20201227
+        if value == pk:
+            break
+    return loop_size
 
 
 def part1():
-    card_pk, door_pk, card_loop_size, door_loop_size = parse_input()
-    return pow(door_pk, card_loop_size, 20201227)
+    card_pk, door_pk = [int(l) for l in open("input.txt").readlines()]
+    return pow(door_pk, get_loop_size(card_pk), 20201227)
 
 
 print(f"Part 1: {part1()}")
