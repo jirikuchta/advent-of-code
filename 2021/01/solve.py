78f4dd44f92bbdfea80b77932be21f2a2de71f69
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-12-02 15:07:50 +0100

    2021/02

--- 2021/01/solve.py
+++ 2021/01/solve.py
@@ -2,19 +2,19 @@
 # https://adventofcode.com/2021/day/1
 
 
-def get_input():
-    return [int(line) for line in open("input.txt")]
+def get_data():
+    return [line.split() for line in open("input.txt")]
 
 
-def part1(numbers):
-    return sum(map(lambda a, b: b > a, numbers, numbers[1:]))
+def part1(data):
+    return sum(map(lambda a, b: b > a, data, data[1:]))
 
 
-def part2(numbers):
-    groups = map(lambda i: numbers[i:i+3], range(0, len(numbers)))
+def part2(data):
+    groups = map(lambda i: data[i:i+3], range(0, len(data)))
     sums = [sum(g) for g in groups if len(g) == 3]
     return part1(sums)
 
 
-print(part1(get_input()))
-print(part2(get_input()))
+print(part1(get_data()))
+print(part2(get_data()))
---
+++ 2021/02/solve.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# https://adventofcode.com/2021/day/1
+
+
+def get_data():
+    return list(map(lambda i: [i[0], int(i[1])],
+                    [line.split() for line in open("input.txt")]))
+
+
+def part1(data):
+    x = map(lambda i: i[1],
+            filter(lambda i: i[0] == "forward", data))
+    y = map(lambda i: -i[1] if i[0] == "up" else i[1],
+            filter(lambda i: i[0] != "forward", data))
+    return sum(x) * sum(y)
+
+
+def part2(data):
+    x, y, aim = [0, 0, 0]
+    for direction, steps in data:
+        if direction == "forward":
+            x += steps
+            y += aim * steps
+        else:
+            aim = aim + (-steps if direction == "up" else steps)
+    return x * y
+
+
+print(part1(get_data()))
+print(part2(get_data()))
