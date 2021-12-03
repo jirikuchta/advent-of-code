0fb68cd1e43b02290eeb82024d46ab702b199194
jirikuchta <jiri.kuchta@firma.seznam.cz>
2021-12-03 23:31:22 +0100

    upadte 2021/03

--- 2021/03/solve.py
+++ 2021/03/solve.py
@@ -8,13 +8,17 @@ def get_data():
     return [ln.strip() for ln in open("input.txt")]
 
 
-def part1(data, mode):
-    data = [Counter([i[pos] for i in data]) for pos in range(0, len(data[0]))]
-    res = [c.most_common()[0 if mode == "gamma" else -1][0] for c in data]
+def part1(mode):
+    data = get_data()
+    counters = [Counter([i[pos] for i in data])
+                for pos in range(0, len(data[0]))]
+    res = [c.most_common()[0 if mode == "gamma" else -1][0] for c in counters]
     return int("".join(res), 2)
 
 
-def part2(data, mode):
+def part2(mode):
+    data = get_data()
+
     for pos in range(0, len(data[0])):
         if len(data) == 1:
             break
@@ -30,5 +34,5 @@ def part2(data, mode):
     return int(data[0], 2)
 
 
-print(part1(get_data(), "gamma") * part1(get_data(), "epsilon"))
-print(part2(get_data(), "oxygen") * part2(get_data(), "co2"))
+print(part1("gamma") * part1("epsilon"))
+print(part2("oxygen") * part2("co2"))
