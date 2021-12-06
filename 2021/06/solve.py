6fe13bba20f11ebb3c61b0636a46e5ff431781b6
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-12-06 16:31:23 +0100

    2021/06

---
+++ 2021/06/solve.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# https://adventofcode.com/2021/day/6
+
+from collections import Counter
+
+
+def count(rounds):
+    c = Counter([int(n) for n in open("input.txt").read().split(",")])
+    numbers = [c.get(i) or 0 for i in range(0, 9)]
+
+    for i in range(rounds):
+        growth = numbers[0]
+        numbers.pop(0)
+        numbers[6] += growth
+        numbers.append(growth)
+
+    return sum(numbers)
+
+
+print(count(80))
+print(count(256))
