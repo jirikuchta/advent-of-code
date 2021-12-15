ce18f6fe49cbaa509933b79a58df331987a8fc86
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-12-15 22:48:55 +0100

    2021/15 part2

--- 2021/15/solve.py
+++ 2021/15/solve.py
@@ -4,49 +4,61 @@
 from math import inf
 from queue import PriorityQueue
 
-vertices = {}
-start_vertex = complex()
-end_vertex = complex()
 
-for y, row in enumerate(open("input.txt").read().strip().split("\n")):
-    for x, risk in enumerate(row):
-        vertices[complex(y, x)] = int(risk)
-        end_vertex = complex(y, x)
+def dijkstra(grid):
+    start = (0, 0)
+    end = (len(grid) - 1, len(grid[0]) - 1)
 
-results = {k: inf for k in vertices.keys()}
-results[start_vertex] = 0
+    q = PriorityQueue()
+    q.put((grid[start[0]][start[1]], start))
 
-q = PriorityQueue()
-q.put((vertices[start_vertex], str(start_vertex)))
+    results = {}
+    for row, _ in enumerate(grid):
+        for col, _ in enumerate(grid[row]):
+            results[(row, col)] = inf
+    results[start] = 0
 
-visited = []
+    visited = set()
 
-while not q.empty():
-    _, vertex = q.get()
-    vertex = complex(vertex)
+    while not q.empty():
+        vertex = q.get()[1]
+        visited.add(vertex)
 
-    visited.append(vertex)
+        for move in (-1, 0), (0, 1), (1, 0), (0, -1):
+            row = vertex[0] + move[0]
+            col = vertex[1] + move[1]
+            neighbor = (row, col)
 
-    neighbors = [
-        vertex + complex("1"),
-        vertex + complex("1j"),
-        vertex + complex("-1"),
-        vertex + complex("-1j")
-    ]
+            if neighbor in visited:
+                continue
 
-    for neighbor in neighbors:
-        if neighbor not in vertices:
-            continue
+            try:
+                old_risk = results[neighbor]
+                new_risk = results[vertex] + grid[row][col]
+            except KeyError:
+                continue  # out of grid
 
-        if neighbor in visited:
-            continue
+            if new_risk < old_risk:
+                q.put((new_risk, neighbor))
+                results[neighbor] = new_risk
 
-        old_risk = results[neighbor]
-        new_risk = results[vertex] + vertices[neighbor]
+    return results[end]
 
-        if new_risk < old_risk:
-            q.put((new_risk, str(neighbor)))
-            results[neighbor] = new_risk
 
-# part1
-print(results[end_vertex])
+grid = [list(map(int, row.strip())) for row in open("input.txt")]
+print(f"Part1: {dijkstra(grid)}")
+
+rows_count = len(grid)
+cols_count = len(grid[0])
+
+for _ in range(4):
+    for row in grid:
+        tail = row[-rows_count:]
+        row.extend((x + 1) if x < 9 else 1 for x in tail)
+
+for _ in range(4):
+    for row in grid[-cols_count:]:
+        row = [(x + 1) if x < 9 else 1 for x in row]
+        grid.append(row)
+
+print(f"Par2: {dijkstra(grid)}")
