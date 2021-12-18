97e54ca19f1749ae0d605de5dfb32703159931b2
jirikuchta <jiri.kuchta@firma.seznam.cz>
2021-12-18 01:42:44 +0100

    2021/16 part2

--- 2021/16/solve.py
+++ 2021/16/solve.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # https://adventofcode.com/2021/day/16
 
+
+from math import prod
+from operator import gt, lt, eq
+op = [sum, prod, min, max, lambda v: v[0], gt, lt, eq]
+
+
 class BitStream:
     total_version = 0
+    total_value = 0
 
     def __init__(self, data):
         self.data = data
@@ -10,25 +17,31 @@ class BitStream:
     def handle(self):
         BitStream.total_version += int(self.read(3), 2)
         tid = int(self.read(3), 2)
+        values = []
 
         if tid == 4:
-            self.read_literal()
+            values.append(self.read_literal())
         else:
             if self.read(1) == "0":
                 bs = BitStream(self.read(int(self.read(15), 2)))
                 while True:
                     try:
-                        bs.handle()
+                        values.append(bs.handle())
                     except Exception:
                         break
             else:
                 for i in range(int(self.read(11), 2)):
-                    self.handle()
+                    values.append(self.handle())
+
+        if tid < 5:
+            return op[tid](values)
+        else:
+            return op[tid](*values)
 
     def read(self, pos):
-        value = self.data[:pos]
+        res = self.data[:pos]
         self.data = self.data[pos:]
-        return value
+        return res
 
     def read_literal(self):
         v, c = "", "1"
@@ -38,5 +51,6 @@ class BitStream:
         return int(v, 2)
 
 
-BitStream(bin(int("1"+open("input.txt").read().strip(), base=16))[3:]).handle()
+res = BitStream(bin(int("1"+open("input.txt").read().strip(), base=16))[3:]).handle()
 print(f"part1: {BitStream.total_version}")
+print(f"part2: {res}")
