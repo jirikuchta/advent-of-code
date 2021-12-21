de4738f94016fd7fe02584125289b7f0f87e5469
Jiri Kuchta <jiri.kuchta@firma.seznam.cz>
2021-12-21 13:43:21 +0100

    2021/20

---
+++ 2021/20/solve.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# https://adventofcode.com/2021/day/20
+
+
+key_data, img_data = open("input.txt").read().split("\n\n")
+
+key = key_data.replace("\n", "")
+img = [[col for col in line] for line in img_data.strip().split("\n")]
+
+
+def display(grid):
+    return "\n".join(["".join(map(str, row)) for row in grid])
+
+
+def decode(data):
+    bn = "".join(["".join(map(str, row)) for row in data])
+    n = int(bn.replace(".", "0").replace("#", "1"), 2)
+    return key[n]
+
+
+def pad(img, v):
+    for i in range(3):
+        img.insert(0, [v]*len(img[0]))
+        img.append([v]*len(img[0]))
+        for row in img:
+            row.insert(0, v)
+            row.append(v)
+    return img
+
+
+def enhance(img, steps):
+    for i in range(steps):
+        img = pad(img, "#" if i % 2 == 1 else ".")
+        enhanced = []
+
+        for y in range(1, len(img) - 1):
+            row = []
+            for x in range(1, len(img[0]) - 1):
+                row.append(decode([
+                    img[y-1][x-1:x+2],
+                    img[y][x-1:x+2],
+                    img[y+1][x-1:x+2]
+                ]))
+            enhanced.append(row)
+        img = enhanced
+    return img
+
+
+print(f"Part1:\n{display(enhance(img, 2)).count('#')}")
+print(f"Part2:\n{display(enhance(img, 50)).count('#')}")
